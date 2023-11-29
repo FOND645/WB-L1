9527c86f4196fb9aa6302f1a9a1a598e9111be79
FOND645 <fond645@gmail.com>
2023-11-29 20:17:48 +0300

    Сделана задача №16

---
+++ task16.js
@@ -0,0 +1,12 @@
+// Задача на модули и использование внешних библиотек: напишите модуль,
+//  который экспортирует функцию для работы с датами. Внутри модуля
+//  используйте внешнюю библиотеку Moment.js для удобной работы с датами.
+
+const Moment = require('moment');
+
+function getFormatDate(date) {
+    date = Moment(date).format('DD of MMMM YYYY HH:MM:SS');
+    return date;
+}
+
+export default getFormatDate;
