b8fca44595e5e93c9b38b84c6f07192540e74c43
FOND645 <112339957+FOND645@users.noreply.github.com>
2023-11-29 09:29:51 +0300

    решена 17 задача

--- task17/index.js
+++ task17/index.js
@@ -10,35 +10,71 @@ const API_S_KEY = `130c894c-e50b-4bef-b391-2a6f33e4c172`
 const API_RES_FORMAT = 'json'
 const API_URL = 'https://geocode-maps.yandex.ru/1.x/'
 
-const getGeoCodeURL = (text) =>`https://geocode-maps.yandex.ru/1.x/?apikey=${API_S_KEY}&geocode=${text}&format=json`
+const getGeoCodeURL = (text) => `https://geocode-maps.yandex.ru/1.x/?apikey=${API_S_KEY}&geocode=${text}&format=json`
 
-function fetchAddresPromise(text, callback) {
-    return new Promise((res, rej) => {
-        fetch(getGeoCodeURL(text))
-            .then(response => response.json())
-            .then(data => {
-                const result = data.response.GeoObjectCollection.featureMember.map(Element => Element.GeoObject.metaDataProperty.GeocoderMetaData.text)
-                console.log(result)
-                if (callback) callback(result)
-                res(result)
-            })
-            .catch(error => console.log("error", error));
-    })
-}
+class GeoCodeAutocompite {
+    constructor(args) {
+        this.inputElement = document.getElementById('input')
+        this.dropDownList = document.getElementById('drop-down-list')
+        this.searchedResult = []
+        this.inputElement.addEventListener('input', this.getInputHandler().bind(this))
+    }
 
-fetchAddresPromise('рязань новаторов 5к3')
+    isVieldEmpty() {
+        return this.inputElement.value === ''
+    }
 
+    getInputHandler() {
+        let timeout = undefined
 
-const inputFieldElement = document.getElementById('input')
-inputFieldElement.addEventListener('focusout', getInputHandler())
-
-function getInputHandler() {
-    let timeOut
-    return (element) => {
-        clearTimeout(timeOut)
-        const callback = result => {
-            element.srcElement.value = result
+        return () => {
+            const text = this.inputElement.value
+            if (text === "") {
+                this.fetchCallback([])
+                return
+            }
+            if (timeout) return
+            clearTimeout(timeout)
+            timeout = setTimeout(() => {
+                this.fetchAddresPromise(text, this.fetchCallback.bind(this))
+                clearTimeout(timeout)
+                timeout = undefined
+            }, 500)
         }
-        timeOut = setTimeout(() => fetchAddresPromise(element.srcElement.value, callback), 500)
+    }
+
+    fetchCallback(result) {
+        this.searchedResult = result
+        const selectTextElements = result.map((Text, Ind) => `<div class="drop-down-element" id="drop-down-element-${Ind}">${Text}</div>`)
+        this.dropDownList.innerHTML = ''
+        this.dropDownList.innerHTML = selectTextElements.join('')
+        selectTextElements.forEach((_, ind) => {
+            const selectElement = document.getElementById(`drop-down-element-${ind}`)
+            selectElement.addEventListener('click', this.getSelectElementClickHandler(ind).bind(this))
+        })
+    }
+
+    fetchAddresPromise(text, callback) {
+        return new Promise((res, rej) => {
+            fetch(getGeoCodeURL(text))
+                .then(response => response.json())
+                .then(data => {
+                    const result = data.response.GeoObjectCollection.featureMember
+                        .map(Element => Element.GeoObject.metaDataProperty.GeocoderMetaData.text)
+                    if (callback) callback(result)
+                    res(result)
+                })
+                .catch(error => console.log("error", error));
+        })
+    }
+
+    getSelectElementClickHandler(index) {
+        return () => {
+            this.inputElement.value = this.searchedResult[index]
+            this.dropDownList.innerHTML = ''
+            this.searchedResult = []
+        }
     }
 }
+
+const geoCodeAutocompite = new GeoCodeAutocompite()
