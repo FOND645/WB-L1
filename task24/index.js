f4118e652920c9070c3572baf279275453f151bb
FOND645 <112339957+FOND645@users.noreply.github.com>
2023-11-30 11:05:24 +0300

    Сделана задача 24

--- task24/index.js
+++ task24/index.js
@@ -1,8 +1,122 @@
 class Table {
     constructor() {
         this.data = [];
+        this.size = 10
+        this.pageNumber = 1
+        this.sortOrder = {
+            column: false,
+            order: false
+        }
+        this.tableElement = document.getElementById('table')
+        this.inputElement = document.getElementById('input')
+        this.decrButtonElement = document.getElementById('page-decrement')
+        this.incrButtonElement = document.getElementById('page-increment')
+        this.decrButtonElement.addEventListener('click', this.getPageButtonsHandler(false).bind(this))
+        this.incrButtonElement.addEventListener('click', this.getPageButtonsHandler(true).bind(this))
+        this.inputElement.addEventListener('input', this.inputFormater.bind(this))
         this.url =
             'http://www.filltext.com/?rows=1000&fname=%7BfirstName%7D&lname=%7BlastName%7D&tel=%7Bphone%7Cformat%7D&address=%7BstreetAddress%7D&city=%7Bcity%7D&state=%7BusState%7Cabbr%7D&zip=%7Bzip%7D&pretty=true';
+        this.fetchData().then(_ => this.drawTable())
+        this.inputElement.value = this.pageNumber
+
+    }
+
+    getPageButtonsHandler(isIncrement) {
+        return () => {
+            console.log('handler')
+            if (isIncrement) {
+                if (+this.inputElement.max === this.pageNumber) return
+                this.pageNumber++
+            } else {
+                if (this.pageNumber === 1) return
+                this.pageNumber--
+            }
+
+            this.inputElement.value = this.pageNumber
+            this.drawTable()
+        }
+    }
+
+    inputFormater() {
+        if (this.inputElement.value % 1 !== 0) this.inputElement.value = Math.trunc(this.inputElement.value)
+        if (this.inputElement.value.toString() === '') return
+        if (+this.inputElement.value < 1 || +this.inputElement.value > +this.inputElement.max) {
+            this.inputElement.value = this.pageNumber
+        }
+        this.pageNumber = this.inputElement.value
+        this.drawTable()
+    }
+
+    getSorterButtonHandler(fieldName) {
+        return () => {
+            if (this.sortOrder.column === fieldName) {
+                if (this.sortOrder.order === "asc") {
+                    this.sortOrder.order = 'desc'
+                } else if (this.sortOrder.order === "desc") {
+                    this.sortOrder.order = false
+                    this.sortOrder.column = false
+                } else this.sortOrder.order = 'asc'
+            } else {
+                this.sortOrder.order = 'asc'
+                this.sortOrder.column = fieldName
+            }
+            this.drawTable()
+        }
+    }
+
+    getTableHeaderHTML() {
+        return [
+            { name: 'fname', title: 'Имя' },
+            { name: 'lname', title: 'Фамилия' },
+            { name: 'tel', title: 'Телефон' },
+            { name: 'address', title: 'Адрес' },
+            { name: 'city', title: 'Город' },
+            { name: 'state', title: 'Штат' },
+            { name: 'zip', title: 'Индекс' }].map(Field => {
+                const { name, title } = Field
+                const symbol = (() => {
+                    if (this.sortOrder.column === name) return this.sortOrder.order === 'asc' ? "↓" : "↑"
+                    return '|'
+                })()
+                return `<p class="table-col-header">${title} <span class="page-button" id="sort-button-${name}">${symbol}</span></p>`
+            }).join('')
+    }
+
+    getTableRowHTML(record) {
+        const { fname, lname, tel, address, city, state, zip } = record
+        return [fname, lname, tel, address, city, state, zip].map(Cell => `<p class="table-cell">${Cell}</p>`).join('')
+    }
+
+    getVisibleTableHTML() {
+        const visibleData = this.getVisibleData(this.size, this.pageNumber)
+        return visibleData.map(Row => this.getTableRowHTML(Row)).join('')
+    }
+
+
+    drawTable() {
+        this.tableElement.innerHTML = this.getTableHeaderHTML() + this.getVisibleTableHTML()
+        this.inputElement.max = Math.trunc(this.data.length / this.size)
+
+        if (+this.inputElement.max === this.pageNumber) {                
+            this.incrButtonElement.style.cursor = 'not-allowed'
+            this.incrButtonElement.style.color = 'gray'
+        } else {
+            this.incrButtonElement.style.cursor = 'pointer'
+            this.incrButtonElement.style.color = 'black'
+        }
+
+        if (this.pageNumber === 1) {            
+            this.decrButtonElement.style.cursor = 'not-allowed'
+            this.decrButtonElement.style.color = 'gray'
+        } else {
+            this.decrButtonElement.style.cursor = 'pointer'
+            this.decrButtonElement.style.color = 'black'
+        }
+        this.sortButtonsElements = ['fname', 'lname', 'tel', 'address', 'city', 'state', 'zip'].map(FieldName => {
+            const Element = document.getElementById(`sort-button-${FieldName}`)
+            Element.addEventListener('click', this.getSorterButtonHandler(FieldName).bind(this))
+            return Element
+        })
     }
 
     fetchData() {
@@ -16,13 +130,27 @@ class Table {
         });
     }
 
+    compareElements(A, B, order) {
+        if (typeof A === "number") A = A.toString()
+        if (typeof B === "number") B = B.toString()
+        const compareIndex = order === 'asc' ? -1 : 1
+        if (A > B) return -1 * compareIndex
+        if (A < B) return 1 * compareIndex
+        return 0
+    }
+
     getVisibleData(size, pageNumber) {
         const start = size * (pageNumber - 1);
         const end = size * pageNumber;
-        return this.data.slice(start, end);
+        if (this.sortOrder.column) return [...this.data].sort((A, B) =>
+            this.compareElements(
+                A[this.sortOrder.column],
+                B[this.sortOrder.column],
+                this.sortOrder.order
+            ))
+            .slice(start, end);
+        return [...this.data].slice(start, end);
     }
 }
 
 const table = new Table();
-
-table.fetchData().then((_) => console.log(table.getVisibleData(5, 4)));
